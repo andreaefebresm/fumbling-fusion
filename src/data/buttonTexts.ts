cc58a1cb2f53d218d8cfda7b714dab1bfe03be22
Andrea Elena Febres Medina <andreae.febresm@gmail.com>
2025-03-31 11:15:12 +0200

    APPLICATION

---
+++ src/data/buttonTexts.ts
@@ -0,0 +1,10 @@
+export const buttonTexts = {
+    coffee: "Coffee",
+    espresso: "Espresso",
+    coldbrew: "Cold brew machine",
+    water: "Water machine",
+    hotwater: "Hot water dispensing",
+    ice: "Ice",
+    steam: "Steam oven",
+    warewasher: "Warewasher"
+  };
